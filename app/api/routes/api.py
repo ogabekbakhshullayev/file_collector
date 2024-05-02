f5332334ded54bc6340731dee65cc0b24d5ffc88
bnutfilloyev <yoshlikmedia@gmail.com>
2024-05-02 14:05:06 +0500

    Some update

--- app/api/routes/api.py
+++ app/api/routes/api.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
 
-from api.routes import file_save_request, depth_to_image
+from api.routes import file_save_request, depth_to_image, near_far_facemesh_save_request
 
 router = APIRouter()
 router.include_router(file_save_request.router, tags=["file-saver"], prefix="/v1")
 router.include_router(depth_to_image.router, tags=["depth-to-image"], prefix="/v1")
+router.include_router(near_far_facemesh_save_request.router, tags=["near-far-facemesh"], prefix="/v1")
--- app/api/routes/near_far_facemesh_save_request.py
+++ app/api/routes/near_far_facemesh_save_request.py
@@ -4,7 +4,6 @@ from models.prediction import ResponseData
 import aiofiles
 from typing import Annotated
 
-from uuid import uuid4
 import os
 
 router = APIRouter()
@@ -15,42 +14,41 @@ async def facemesh_and_image_save(
     near_image: Annotated[UploadFile, "image/jpg"] = File(...),
     far_image: Annotated[UploadFile, "image/jpg"] = File(...),
     facemesh: Annotated[UploadFile, "application/json"] = File(...),
-    file_id: str = Form(...),
+    file_id: str = Body(...)
 ):
     if not near_image or not far_image or not facemesh:
         raise HTTPException(status_code=404, detail="Files not found!")
     
 
     try:
-        os.makedirs(f"app/data/near_image", exist_ok=True)
-        os.makedirs(f"app/data/far_image", exist_ok=True)
-        os.makedirs(f"app/data/facemesh", exist_ok=True)
+        os.makedirs(f"/images/near_image", exist_ok=True)
+        os.makedirs(f"/images/far_image", exist_ok=True)
+        os.makedirs(f"/images/facemesh", exist_ok=True)
 
         # check if file_id already exists remove the files and save it again
-        if os.path.exists(f"app/data/near_image/{file_id}.jpg"):
-            os.remove(f"app/data/near_image/{file_id}.jpg")
+        if os.path.exists(f"/images/near_image/{file_id}.jpg"):
+            os.remove(f"/images/near_image/{file_id}.jpg")
 
-        if os.path.exists(f"app/data/far_image/{file_id}.jpg"):
-            os.remove(f"app/data/far_image/{file_id}.jpg")
+        if os.path.exists(f"/images/far_image/{file_id}.jpg"):
+            os.remove(f"/images/far_image/{file_id}.jpg")
 
-        if os.path.exists(f"app/data/facemesh/{file_id}.json"):
-            os.remove(f"app/data/facemesh/{file_id}.json")
+        if os.path.exists(f"/images/facemesh/{file_id}.json"):
+            os.remove(f"/images/facemesh/{file_id}.json")
             
-
         # Save near image
-        near_image_path = f"app/data/near_image/{file_id}.jpg"
+        near_image_path = f"/images/near_image/{file_id}.jpg"
         async with aiofiles.open(near_image_path, "wb") as out_file:
             content = await near_image.read()
             await out_file.write(content)
 
         # Save far image
-        far_image_path = f"app/data/far_image/{file_id}.jpg"
+        far_image_path = f"/images/far_image/{file_id}.jpg"
         async with aiofiles.open(far_image_path, "wb") as out_file:
             content = await far_image.read()
             await out_file.write(content)
 
         # Save facemesh
-        facemesh_path = f"app/data/facemesh/{file_id}.json"
+        facemesh_path = f"/images/facemesh/{file_id}.json"
         async with aiofiles.open(facemesh_path, "wb") as out_file:
             content = await facemesh.read()
             await out_file.write(content)
