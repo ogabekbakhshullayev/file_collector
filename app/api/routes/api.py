d2fd53292c0535c01cdba4bc4c69e90156c3d517
bnutfilloyev <yoshlikmedia@gmail.com>
2024-11-27 11:58:52 +0500

    Added facepay data collection

--- app/api/routes/api.py
+++ app/api/routes/api.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter
 
-from api.routes import file_save_request, depth_to_image, near_far_facemesh_save_request, video_and_facemesh_save_request, healthcheck
+from api.routes import file_save_request, depth_to_image, near_far_facemesh_save_request, video_and_facemesh_save_request, rgb_ir_landmark_bbox_save_request, healthcheck
 
 router = APIRouter()
 router.include_router(file_save_request.router, tags=["file-saver"], prefix="/v1")
 router.include_router(depth_to_image.router, tags=["depth-to-image"], prefix="/v1")
 router.include_router(near_far_facemesh_save_request.router, tags=["near-far-facemesh"], prefix="/v1")
 router.include_router(video_and_facemesh_save_request.router, tags=["video-facemesh"], prefix="/v1")
+router.include_router(rgb_ir_landmark_bbox_save_request.router, tags=["rgb-ir-landmark-bbox"], prefix="/v1")
 router.include_router(healthcheck.router, tags=["healthcheck"], prefix="/v1")
---
+++ app/api/routes/rgb_ir_landmark_bbox_save_request.py
@@ -0,0 +1,62 @@
+from fastapi import APIRouter, HTTPException, File, UploadFile, Form, Body
+
+from models.prediction import ResponseData
+import aiofiles
+from typing import Annotated
+
+from uuid import uuid4
+import os
+
+router = APIRouter()
+
+facepay_data_dir = "/facepay-data"
+
+@router.post("/rgb-ir-result-save", response_model=ResponseData)
+async def predict(
+    rgb_image: Annotated[UploadFile, File(...)],
+    ir_image: Annotated[UploadFile, File(...)],
+    result: Annotated[UploadFile, Form(...)],
+    status: Annotated[str, Form(...)],
+    file_id: Annotated[str, Form(...)],
+):
+    if not rgb_image or not ir_image or not result:
+        raise HTTPException(status_code=404, detail="Files not found!")
+    
+
+    try:
+        save_dir = f"{facepay_data_dir}/{status}/{file_id}"
+        os.makedirs(save_dir, exist_ok=True)
+
+        rgb_image_path = f"{save_dir}/rgb_image.jpeg"
+        ir_image_path = f"{save_dir}/ir_image.jpeg"
+        result_path = f"{save_dir}/result.json"
+
+        if os.path.exists(rgb_image_path):
+            os.remove(rgb_image_path)
+
+        if os.path.exists(ir_image_path):
+            os.remove(ir_image_path)
+
+        if os.path.exists(result_path):
+            os.remove(result_path)
+
+        async with aiofiles.open(rgb_image_path, "wb") as out_file:
+            content = await rgb_image.read()
+            await out_file.write(content)
+
+        async with aiofiles.open(ir_image_path, "wb") as out_file:
+            content = await ir_image.read()
+            await out_file.write(content)
+
+        async with aiofiles.open(result_path, "wb") as out_file:
+            content = await result.read()
+            await out_file.write(content)
+
+    except Exception as err:
+        raise HTTPException(status_code=500, detail=f"Exception: {err}")
+
+    return ResponseData(
+        status_code=200,
+        file_id=file_id,
+        detail="RGB, IR, and result files saved successfully!",
+    )
